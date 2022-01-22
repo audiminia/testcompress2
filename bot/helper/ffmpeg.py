7ef4424d69c4431fc662bb6b940be7e5c8aeadb8
Audi <76532995+audiminia@users.noreply.github.com>
2022-01-22 19:55:41 +0530

    Test

--- bot/helper/ffmpeg.py
+++ bot/helper/ffmpeg.py
@@ -59,14 +59,14 @@ def get_thumbnail(in_filename, path, ttl):
 
 def get_duration(filepath):
     metadata = extractMetadata(createParser(filepath))
-    if metadata.has("duration"):
+    if "duration" in metadata:
       return metadata.get('duration').seconds
     else:
       return 0
 
 def get_width_height(filepath):
     metadata = extractMetadata(createParser(filepath))
-    if metadata.has("width") and metadata.has("height"):
+    if "width" in metadata and "height" in metadata:
       return metadata.get("width"), metadata.get("height")
     else:
       return 1280, 720
