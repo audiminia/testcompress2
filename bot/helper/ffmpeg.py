dc8422c09fecfb37d57f8099a2da46a8b42940ae
Audi <76532995+audiminia@users.noreply.github.com>
2022-01-23 03:16:31 +0530

    Update ffmpeg.py

--- bot/helper/ffmpeg.py
+++ bot/helper/ffmpeg.py
@@ -41,32 +41,3 @@ async def encode(filepath):
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
-
-def get_thumbnail(in_filename, path, ttl):
-    out_filename = os.path.join(path, str(time.time()) + ".jpg")
-    open(out_filename, 'a').close()
-    try:
-        (
-            ffmpeg
-            .input(in_filename, ss=ttl)
-            .output(out_filename, vframes=1)
-            .overwrite_output()
-            .run(capture_stdout=True, capture_stderr=True)
-        )
-        return out_filename
-    except ffmpeg.Error as e:
-      return None
-
-# def get_duration(filepath):
-#     metadata = extractMetadata(createParser(filepath))
-#     if metadata.has('duration'):
-#       return metadata.get('duration').seconds
-#     else:
-#       return 0
-
-# def get_width_height(filepath):
-#     metadata = extractMetadata(createParser(filepath))
-#     if metadata.has('width') and metadata.has('height'):
-#       return metadata.get("width"), metadata.get("height")
-#     else:
-#       return 1280, 720
