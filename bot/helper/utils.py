a85b981cdd57277e89a9c2a568f7d3f73e196896
Audi <76532995+audiminia@users.noreply.github.com>
2022-01-25 13:09:06 +0530

    Update utils.py

--- bot/helper/utils.py
+++ bot/helper/utils.py
@@ -15,17 +15,17 @@ def add_task(message: Message):
       msg.edit("Encoding.....")
       new_file = encode(filepath)
       if new_file:
-        msg.edit("Video Encoded Successfully\nGetting Metadata 🐭")
+        msg.edit("**Video Encoded Successfully**\nGetting Metadata...")
         duration = get_duration(new_file)
         thumb = get_thumbnail(new_file, download_dir, duration / 4)
         width, height = get_width_height(new_file)
-        msg.edit("Uploading 🐭")
-        message.reply_video(new_file, quote=True, supports_streaming=True, thumb=thumb, duration=duration, width=width, height=height)
+        msg.edit("Uploading.....")
+        message.reply_video(new_file, supports_streaming=True, thumb=thumb, duration=duration, width=width, height=height)
         os.remove(new_file)
         os.remove(thumb)
-        msg.edit("Video Successfully Encoded to x265 🐭")
+        msg.edit("Video Successfully Encoded to x265")
       else:
-        msg.edit("Something Went Wrong While Encoding :(\nTry Again Later 🐭")
+        msg.edit("Something Went Wrong While Encoding \nTry Again Later ;)")
         os.remove(filepath)
     except Exception as e:
       msg.edit(f"```{e}```")
